e67fa392bec845f018e734cd7cba431f16879204
ChinhLC <chinhlc@smartosc.com>
2018-12-21 16:24:40 +0700

    Module Child Component: ng g c name-module/name-childcomponent

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ProductComponent]
+  declarations: [ProductComponent, ProductListComponent, ProductCreateComponent, ProductDetailComponent]
 })
 export class ProductModule { }
