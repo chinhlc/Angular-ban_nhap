787af96aabc5f1d4d9b80b9f84fedecb9b9e4647
ChinhLC <chinhlc@smartosc.com>
2019-01-04 14:27:49 +0700

    Service data-type - v2: Blog Detail

--- src/app/article/blog-detail/blog-detail.component.ts
+++ src/app/article/blog-detail/blog-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { switchMap, map } from 'rxjs/operators';
+
+import {IBlog} from '../../type-data/type-blog';
+import {BlogService} from '../../type-data/data-service/blog.service';
+
 @Component({
   selector: 'app-blog-detail',
   templateUrl: './blog-detail.component.html',
@@ -8,13 +13,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BlogDetailComponent implements OnInit {
 
-  id: string;
+  BlogItem: IBlog;
 
-  constructor(private route: ActivatedRoute) {
-    route.params.subscribe(params => { this.id = params['id']; });
+  // id: string;
+
+  constructor(
+    private blogSV: BlogService,
+    private route: ActivatedRoute) {
+    // route.params.subscribe(params => { this.id = params['id']; });
   }
 
   ngOnInit() {
+    // this.route.paramMap.subscribe(params => {
+    //   const id = params.get('id');
+    //   this.productService.findBlogById(id).subscribe(
+    //     product => this.product = product
+    //   );
+    // });
+    this.route.paramMap.pipe(
+      map(params => params.get('id')),
+      switchMap(id => this.blogSV.findBlogById(id))
+    ).subscribe(dt => this.BlogItem = dt);
+    // const paramMap = this.route.snapshot.paramMap;
+    // const id = paramMap.get('id');
+    // this.productService.findBlogById(id).subscribe(product => this.product = product);
   }
 
 }
