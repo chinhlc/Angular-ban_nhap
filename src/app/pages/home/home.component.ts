4f4da0a55d2dc0851d7a6c03b0050caa8eb71653
ChinhLC <chinhlc@smartosc.com>
2019-01-08 14:56:51 +0700

    Share-module: helper number

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {UserService} from '../../share-module/dependency-injection/user.service';
+import {NumberHelper} from '../../share-module/helper/number-helper';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,14 @@ import {UserService} from '../../share-module/dependency-injection/user.service'
 })
 export class HomeComponent implements OnInit {
 
+  round = 20.0566;
+  isnumber: boolean;
+
   constructor(public userService: UserService) { }
 
   ngOnInit() {
+    this.round  = NumberHelper.round(this.round, 3);
+    this.isnumber  = NumberHelper.integer(1805.66);
   }
 
 }
---
+++ src/app/share-module/helper/number-helper.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class NumberHelper {
+  public static round(price: number, places: number = 2): number {
+    return +(Math.round(parseFloat(parseFloat(price + "") + "e+" + places)) + "e-" + places);
+  }
+
+  public static integer(number: any) {
+    return Number.isInteger(number);
+  }
+}
